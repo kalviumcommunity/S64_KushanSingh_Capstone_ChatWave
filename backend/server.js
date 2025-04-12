509f3fc7ae00278c19713462144e903a26049aee
KushanSingh-Kalvium <kushansingh890@gmail.com>
2025-04-12 13:59:00 +0530

    Minor Tweaks

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,30 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
-//Routes
+// Import routes
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const conversationRoutes = require("./routes/conversationRoutes");
+
+// Use routes
+app.use("/api/users", userRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/conversations", conversationRoutes);
+
+// Default route
 app.get("/", (req, res) => {
   res.send("🌊 ChatWave backend is live!");
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
